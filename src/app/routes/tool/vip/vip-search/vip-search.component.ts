db2f5602c02c0047f6b5169dd75e673e6790ab59
ZT94 <zt94@Mac.com>
2019-09-17 22:20:24 +0700

    Add layout search vip

--- src/app/routes/tool/vip/vip-search/vip-search.component.ts
+++ src/app/routes/tool/vip/vip-search/vip-search.component.ts
@@ -8,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class VipSearchComponent implements OnInit {
 
   searchInputValue = {
-    name: ''
+    name: null
+  };
+
+  listOfData = {
+    img: 'https://vcdn-vnexpress.vnecdn.net/2019/07/14/Honda-Winner-X-2019-VnE-3437-1563083779_680x0.jpg',
+    title: 'Nuoc mat em lau bang nguoi yeu moi',
+    language: 'EN/US',
+    username: 'Phan Thanh Winner',
+    duration: 150,
+    view: 150,
+    timestamp: '1568733293412'
   };
 
   constructor() { }
